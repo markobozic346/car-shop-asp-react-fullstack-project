e4c66cf728049113bb208577c14b6e329fe8c2e2
markobozic346 <markobozic346@gmail.com>
2024-06-23 18:31:07 +0200

    Add migrations for car price

---
+++ Migrations/20240623154452_AddPriceToCar.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace servis_automobila.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriceToCar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Cars",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Cars");
+        }
+    }
+}
